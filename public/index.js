18964faab78e00fe9a4d600c7fc0848913842129
Curtis Brauner <crbs1234@gmail.com>
2022-11-03 09:57:06 -0400

    further developemtn

--- public/index.js
+++ public/index.js
@@ -39,4 +39,86 @@ window.addEventListener('DOMContentLoaded', function () {
       var user;
       window.location.hash = 'welcome';
       var contactReady = true;
-     
+    var guests, guestList;
+
+    function listGuests () {
+        var guest, guestVals;
+        get(ref(db,'/guests')),((snapshot) => {
+            if (snapshot.exists()) {
+                guests = snapshot.val(); guestVals = Object.values(guests);
+                console.log(guests);
+                for (let i=0; i<guestVals; i++)  {
+                    guest = guestVals[i];
+                    guestList[i] = guest;
+                    console.log(guest + ' loaded');
+                }
+            } else {
+                if (dblChk('Reload Dylan & Curtis only to guest list')) {
+                console.log('setting guests');
+                set(ref(db,'/guests'),{
+                    'dylan': {
+                        'name':'Dylan',
+                        'phone':'516-242-0709',
+                        'role':'Groom',
+                        'email': 'dylangordon999@gmail.com'
+                    },'curtis':{
+                        'name':'Curtis',
+                        'phone':'781-244-7604',
+                        'role':'Best Man',
+                        'email':'crbs1234@gmail.com'
+                    }
+                });
+                listGuests(); 
+                } else {
+                    alert('check the guest list, bitch');
+                }
+            }
+            });
+            console.log(guestList);
+    }
+
+    function dblChk (question) {
+        var answer;
+        if (confirm(question)) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return 
+    }
+
+    function addGuests (id, name, phone, role, email) {
+        var nameID = id;
+        set(ref(db,'/guests' + nameID),{
+                'name':name,
+                'phone':phone,
+                'role':role,
+                'email':email
+        })
+    }
+
+    var picCount = 0;
+
+    function addPicToScroll (pic) {
+        var newPicInput = document.createElement("input");
+        makeElements (newPicInput, "carousel-" + picCount+1, null,"carousel","carousel-open","type","radio", "hidden","");
+        var newPicDiv = document.createElement("div");
+        newPicDiv.setAttribute("class","carousel-item");
+        var newPicImg = document.createElement("img");
+        newPicImg.setAttribute("src","'pictures/"+pic);
+        newPicDiv.appendChild(newPicImg);
+        document.getElementById("carousel-inner-ID").appendChild(newPicInput);
+        document.getElementById("carousel-inner-ID").appendChild(newPicDiv);
+    }
+    
+    
+    listGuests();     
+
+    function makeElements (newEl, id, inHTML, name, clss, rand, randVal, rand2, rand2Val) {
+        if (id != undefined) {newEl.setAttribute('id', id); }
+        if (inHTML != undefined) {newEl.innerHTML = inHTML; }
+        if (name != undefined) {newEl.setAttribute('name', name); }
+        if (clss != undefined) {newEl.setAttribute('class', clss); }
+        if (rand != undefined) {newEl.setAttribute(rand, randVal); }
+        if (rand != undefined) {newEl.setAttribute(rand2, rand2Val); }
+      }
